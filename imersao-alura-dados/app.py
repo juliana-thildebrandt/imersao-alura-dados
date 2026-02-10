fa025f45ddd0c455a00c1e02e31a0af8cccd4c54
Juliana Hildebrandt <jthildebrandt@gmail.com>
2026-02-10 19:45:44 -0300

    Adiciona arquivo app.py do dashboard

---
+++ imersao-alura-dados/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# --- Configuração da Página ---
+# Define o título da página, o ícone e o layout para ocupar a largura inteira.
+st.set_page_config(
+    page_title="Dashboard de Salários na Área de Dados",
+    page_icon="📊",
+    layout="wide",
+)
+# --- Carregamento dos dados ---
+df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+
+# --- Barra Lateral (Filtros) ---
